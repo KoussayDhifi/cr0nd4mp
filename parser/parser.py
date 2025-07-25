d09014187feb3df217a7bedb2f029c1cf5277d25
KoussayDhifi <koussaydhifi48@gmail.com>
2025-07-25 12:19:56 +0100

    Add log entry type distinguish

--- parser/parser.py
+++ parser/parser.py
@@ -4,14 +4,20 @@ from .cronJob import CronJob
 
 
 def detect_log_format(line: str) -> str:
+
     iso8601_start = re.match(r'^\d{4}-\d{2}-\d{2}T', line)
     bsd_start = re.match(r'^[A-Z][a-z]{2}\s+\d{1,2}\s+\d{2}:\d{2}:\d{2}', line)
 
     if iso8601_start:
+
         return "iso"
+
     elif bsd_start:
+
         return "bsd"
+
     else:
+
         return "unknown"
 
 
@@ -26,10 +32,12 @@ def extract_cron_iso (log_splitted: list[str]) -> CronJob:
     	user = log_splitted[3][log_splitted[3].find("(")+1:log_splitted[3].find(")")],
     	command = ' '.join(log_splitted[5:]),
     	hostname =	log_splitted[1],
-    	pid = log_splitted[2][log_splitted[2].find("[")+1:log_splitted[2].find("]")]
+    	pid = log_splitted[2][log_splitted[2].find("[")+1:log_splitted[2].find("]")],
+    	log_type = log_splitted[4]
 	)
 
 def extract_cron_bsd (log_splitted: list[str]) -> CronJob:
+
 	date_string = ' '.join(log_splitted[0:2])
 	date_string = str(datetime.now().year)+' '+date_string
 	time_string = log_splitted[2]
@@ -40,11 +48,12 @@ def extract_cron_bsd (log_splitted: list[str]) -> CronJob:
 		user = log_splitted[5][log_splitted[5].find("(")+1:log_splitted[5].find(")")],
 		command = ' '.join(log_splitted[7:]),
 		hostname = log_splitted[3],
-		pid = log_splitted[4][log_splitted[4].find("[")+1:log_splitted[4].find("]")]
+		pid = log_splitted[4][log_splitted[4].find("[")+1:log_splitted[4].find("]")],
+		log_type = log_splitted[6]
 	) 
 
 
-def extract_cron_entries (file_path: str="/var/log/syslog") -> list[CronJob]:
+def extract_cron_entries (file_path: str="/var/log/syslog", log_entry_types: list[str]=["CMD"]) -> list[CronJob]:
 
 	log_format = ""
 	cron_entries = list()
@@ -59,30 +68,36 @@ def extract_cron_entries (file_path: str="/var/log/syslog") -> list[CronJob]:
 				log_format = detect_log_format(log.strip())
 
 
-
-
 			if log_format == "iso" :
 				
 				if "CRON" in log_splitted[2].upper():	
+
 					job = extract_cron_iso(log_splitted)
-					print(job)
-					cron_entries.append(job)
+
+					if (job.log_type in log_entry_types):
+
+						cron_entries.append(job)
 					
 
 			elif log_format == "bsd":
 				try:
+
 					if "CRON" in log_splitted[4].upper():
+
 						job = extract_cron_bsd(log_splitted)
-						print(job)
-						cron_entries.append(job)
+
+						if (job.log_type in log_entry_types):
+
+							cron_entries.append(job)
+
 				except Exception:
+
 					continue
 
 			else:
+
 				raise Exception("The syslog file has an undefined structure")
 
 			
 
 	return cron_entries
-
-extract_cron_entries("/home/mohsen2/Documents/cr0nd4mp/test.txt")
